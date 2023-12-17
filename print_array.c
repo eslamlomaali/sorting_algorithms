1a12729362906718361e6562a86cae80c086fe2a
Youssef-Essam-Fathy <youssefessam5623@email.com>
2023-12-17 15:48:05 +0200

    sorting_algorithms

--- print_array.c
+++ print_array.c
@@ -15,9 +15,11 @@ void print_array(const int *array, size_t size)
 	while (array && i < size)
 	{
 	if (i > 0)
+	{
 		printf(", ");
 		printf("%d", array[i]);
 		++i;
 	}
 	printf("\n");
+}
 }
--- print_list.c
+++ print_list.c
@@ -14,10 +14,12 @@ void print_list(const listint_t *list)
 	while (list)
 	{
 	if (i > 0)
+	{
 		printf(", ");
 		printf("%d", list->n);
 		++i;
 		list = list->next;
 	}
 	printf("\n");
+}
 }
