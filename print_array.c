908c6397f8fcd0fee92628fe267f5010d940f871
eslamlomaali <eslam@gmail.com>
2023-12-19 11:12:45 -0800

    done

--- print_array.c
+++ print_array.c
@@ -14,12 +14,10 @@ void print_array(const int *array, size_t size)
 	i = 0;
 	while (array && i < size)
 	{
-	if (i > 0)
-	{
-		printf(", ");
+		if (i > 0)
+			printf(", ");
 		printf("%d", array[i]);
 		++i;
 	}
 	printf("\n");
-}
 }
