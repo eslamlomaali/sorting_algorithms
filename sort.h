34abb76e29ee06d8779cc1b4f86e6751bc3af887
eslamlomaali <eslam@gmail.com>
2023-12-19 11:59:10 -0800

    done

--- sort.h
+++ sort.h
@@ -36,5 +36,8 @@ void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void shell_sort(int *array, size_t size);
+void recursion_sort(int *array, size_t size, int start, int end);
+int Lomuto_partition_scheme(int *array, size_t size, int start, int end);
+void swapi(int *int1, int *int2);
 
 #endif /*SORT_H*/
