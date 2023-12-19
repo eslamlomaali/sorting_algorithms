b6cfb2d0708aa6c68132a57fc06a0c14b05fa72b
eslamlomaali <eslam@gmail.com>
2023-12-19 11:30:02 -0800

    done

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -1,20 +1,22 @@
 #include "sort.h"
 
+#include "sort.h"
+
 /**
-*swap_elements - place of two elements into an array
-*@arr: an array
-*@first: an array of element
-*@seconed: an array of element
-*/
-void swap_elements(int *arr, int first, int seconed)
+ * swapi - Swap two integer.
+ * @int1: The first integr
+ * @int2: The second integer
+ *
+ */
+void swapi(int *int1, int *int2)
 {
+	int temp;
 
-	int store;
-
-	store = arr[first];
-	arr[first] = arr[seconed];
-	arr[seconed] = store;
+	temp = *int1;
+	*int1 = *int2;
+	*int2 = temp;
 }
+
 /**
  * shell_sort - rite a function that sorts an array of integers in ascending
  * order using the Shell sort algorithm, using the Knuth sequence
@@ -23,19 +25,25 @@ void swap_elements(int *arr, int first, int seconed)
  */
 void shell_sort(int *array, size_t size)
 {
-	size_t distance = 1, z, idx = 0;
+	size_t distance, x, y;
 
 	if (array == NULL || size < 2)
 		return;
-	while (distance < size / 3)
-		distance = 3 * distance + 1;
-	while (distance >= 1)
+
+	for (distance = 1; distance < (size / 3);)
+		distance = distance * 3 + 1;
+
+	for (; distance >= 1; distance /= 3)
 	{
-		for (z = distance; z < size; z++)
-			for (idx = z; idx >= distance &&
-			 (array[idx] < array[idx - distance]); idx -= distance)
-				swap_elements(array, idx, idx - distance);
+		for (x = distance; x < size; x++)
+		{
+			y = x;
+			while (y >= distance && array[y - distance] > array[y])
+			{
+				swapi(array + y, array + (y - distance));
+				y -= distance;
+			}
+		}
 		print_array(array, size);
-		distance /= 3;
 	}
 }
